5ef0bae7a55b96a0945dcd0f80ed9150fc2f92ef
Douglas G. Moore <doug@dglmoore.com>
2018-11-18 03:57:40 -0700

    Create the Strings factory

---
+++ test/strings.test.js
@@ -0,0 +1,27 @@
+const fs = require('fs-extra');
+const path = require('path');
+const SectionMap = require('../src/sectionmap');
+const Strings = require('../src/strings');
+
+const is_abf = (filename) => path.extname(filename) === '.abf';
+
+const build_table = function() {
+    const data_dir = path.join('test', 'data');
+    const abf_files = fs.readdirSync(data_dir).filter(is_abf);
+    const json_files = abf_files.map((f) => path.basename(f, '.abf') + '-strings.json');
+
+    return abf_files.map(function(abf_file, idx) {
+        const abf_path = path.join(data_dir, abf_file);
+        const json_path = path.join(data_dir, json_files[idx]);
+        const data = fs.readFileSync(abf_path);
+
+        const got = Strings(data, SectionMap(data));
+        const expected = JSON.parse(fs.readFileSync(json_path));
+
+        return [abf_path, got, expected];
+    });
+};
+
+test.each(build_table())('.strings(%s)', function(filename, got, expected) {
+    expect(got).toMatchObject(expected);
+});
