2253af00da98de715ab32df2c226d17cde6a2bcc
Douglas G. Moore <doug@dglmoore.com>
2018-11-17 23:24:32 -0700

    Rename header json files

--- test/header.test.js
+++ test/header.test.js
@@ -7,7 +7,7 @@ const is_abf = (filename) => path.extname(filename) === '.abf';
 const build_table = function() {
     const data_dir = path.join('test', 'data');
     const abf_files = fs.readdirSync(data_dir).filter(is_abf);
-    const json_files = abf_files.map((f) => path.basename(f, '.abf') + '.json');
+    const json_files = abf_files.map((f) => path.basename(f, '.abf') + '-header.json');
 
     return abf_files.map(function(abf_file, idx) {
         const abf_path = path.join(data_dir, abf_file);
