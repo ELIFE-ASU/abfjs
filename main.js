4f8c7d861d01a39b3f77c785f02e239765ede8ad
Douglas G. Moore <doug@dglmoore.com>
2018-11-20 00:29:04 -0700

    Read ABF file into memory and check format

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ const signale = require('signale');
 const path = require('path');
 const fs = require('fs-extra');
 
+const abf_file_format = function(data) {
+    const fmt = data.toString('ascii', 0, 4);
+    return (fmt === 'ABF ') ? 1 : (fmt === 'ABF2') ? 2 : 0;
+};
+
 module.exports = function(filepath, verbosity=0) {
     if (verbosity) signale.start('Reading ABF file');
 
@@ -17,6 +22,15 @@ module.exports = function(filepath, verbosity=0) {
     if (verbosity) {
         signale.info(`ID: ${abf.id}`);
         signale.info(`Filename: ${abf.filepath}`);
+    }
+
+    const data = fs.readFileSync(abf.filepath);
+
+    abf.version = { major: abf_file_format(data) };
+    if (abf.version.major === 0) {
+        throw new Error('Invalid ABF file format');
+    } else if (abf.version.major !== 2) {
+        throw new Error(`Unsupported ABF file format: ${abf.version}`);
     }
 
     return abf;
--- test/abf.test.js
+++ test/abf.test.js
@@ -3,15 +3,26 @@ const path = require('path');
 
 const DATA_DIR = path.join('test', 'data');
 const TEST_FILE = path.join(DATA_DIR, '18710002.abf');
+let abf = null;
 
-test('ABF raises on FileNotExists', () => {
+beforeAll(() => { abf = ABF(TEST_FILE); });
+
+test('Raises on FileNotExists', () => {
     expect(() => ABF('file.abf')).toThrow(Error);
 });
 
-test('ABF sets filepath', () => {
-    expect(ABF(TEST_FILE)).toMatchObject({
-        filepath: path.resolve(TEST_FILE),
-    });
+test('Sets filepath', () => expect(abf).toMatchObject({
+    filepath: path.resolve(TEST_FILE)
+}));
+
+test('Sets ID', () => expect(abf).toMatchObject({
+    id: '18710002'
+}));
+
+test('Throws on invalid format', () => {
+    expect(() => ABF(__filename)).toThrow('Invalid');
 });
 
-test('ABF sets ID', () => expect(ABF(TEST_FILE)).toMatchObject({ id: '18710002' }));
+test('Sets version', () => expect(abf).toMatchObject({
+    version: { major: 2 }
+}));
